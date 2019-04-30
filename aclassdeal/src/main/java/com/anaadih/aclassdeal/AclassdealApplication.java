b03c53239fb74632b4b54b42b8cb1230a0c53b0a
Sanchit Khurana <sanchit007khurana@gmail.com>
2019-04-30 21:57:17 +0530

    Initial Setup

---
+++ aclassdeal/src/main/java/com/anaadih/aclassdeal/AclassdealApplication.java
@@ -0,0 +1,13 @@
+package com.anaadih.aclassdeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AclassdealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AclassdealApplication.class, args);
+	}
+
+}
